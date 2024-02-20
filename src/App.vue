24c8c4a99a1d48c19bcf87780f618ebe6b3393d4
beyerperez123@gmail.com <beyerperez123@gmail.com>
2024-02-20 13:00:39 -0600

    ⭐Feat: Athor Name Create the other name

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import OtherName from './components/icons/OtherName.vue';
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
+    <OtherName />
     <TheWelcome />
   </main>
 </template>
---
+++ src/components/icons/OtherName.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="div">
+        <h1>Athor Name</h1>
+        <p>
+            <a href="https://github.com/GagoPianisst">
+                Beyer Perez Nestor Gael 
+            </a>
+        </p>
+    </div>
+</template>
