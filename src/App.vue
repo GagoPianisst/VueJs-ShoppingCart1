c282f0f556ac3b11e068a2a4d477e654e54bfee1
beyerperez123@gmail.com <beyerperez123@gmail.com>
2024-03-01 11:49:35 -0600

    ⭐Feat: List Rendering Add a list of items using v for

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,21 @@ import { ref } from 'vue'
 //Creando un tipo de referencia reactiva de tipo string
 const header = ref('App Lista de Compras');
 const shoppingIcon = ref ('material-icons shopping-cart-icon');
+// Creando una referencia reactiva para almacenar el valor de la lista
+const items = ref ([
+  {id: 0, label:'Leche'}, 
+  {id: 1, label:'Arroz'}, 
+  {id: 2, label:'Carne'}, 
+  {id: 3, label:'Pan'}, 
+  {id: 4, label:'Huevos'}
+  ]);
 </script>
 
 <template>
   <h1><i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
+  <ul>
+   <li v-for="item in items" v-bind:key="item.id">⭐ {{ item.label }}</li>
+  </ul>
 </template>
 
 <style scoped>
