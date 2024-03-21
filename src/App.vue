9828b6c66ba86a4d051f41d9a66ec50400083164
beyerperez123@gmail.com <beyerperez123@gmail.com>
2024-03-21 12:19:42 -0600

    ⭐FEAT: Highpriority

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,11 @@ const header = ref('App Lista de Compras');
 const shoppingIcon = ref ('material-icons shopping-cart-icon');
 // Creando una referencia reactiva para almacenar el valor de la lista
 const items = ref ([
-  {id: 0, label:'Leche', purchased: false, highPriority: true}, 
-  {id: 1, label:'Arroz', purchased: false, highPriority: false }, 
-  {id: 2, label:'Carne', purchased: true, highPriority: false}, 
-  {id: 3, label:'Pan', purchased: false, highPriority: true}, 
-  {id: 4, label:'Huevos', purchased: true, highPriority: true}
+  // {id: 0, label:'Leche', purchased: false, highPriority: true}, 
+  // {id: 1, label:'Arroz', purchased: false, highPriority: false }, 
+  // {id: 2, label:'Carne', purchased: true, highPriority: false}, 
+  // {id: 3, label:'Pan', purchased: false, highPriority: true}, 
+  // {id: 4, label:'Huevos', purchased: true, highPriority: true}
   ]);
   const togglePurchased = (item) => {
     item.purchased = !item.purchased
@@ -21,14 +21,16 @@ const items = ref ([
   // Metodos
   const saveItems = () => {
     // Agrega un nuevo elemento a la lista proveniente de la caja de texto
-    items.value.push({id: items.value.length, label: newItem.value})
+    items.value.push({id: items.value.length, label: newItem.value, highPriority: newItemHightPriority.value})
     // Borramos el contenido de la caja de texto.
     newItem.value=""
+    newItemHightPriority.value="false";
     
   };
 
   const cancelAddItem = () => {
     showForm.value = false;
+    newItemHightPriority.value="false";
   };
 
   const showAddItemForm = () => {
